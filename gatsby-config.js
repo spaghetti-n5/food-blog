deb80db5bf39036f6aa0ee1cc7fa2c1b8c1d9d55
Eleonora <eleonora.baret@gmail.com>
2021-02-23 13:13:05 +0100

    Added home page with index of markdown pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Cucina dal Nord`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Eleonora Baret`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,56 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby";
 
+import SEO from "../components/seo";
 import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+export default function Home({ data }) {
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>Tutte le ricette</h1>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => {
+          const postSlug = node.frontmatter.title.toLowerCase().replace(/\s/g, '-');
+          return (
+            <Link to={postSlug} style={{textDecoration: 'none'}}>
+              <div key={node.id}>
+                <h3>
+                  {node.frontmatter.title}{" "}
+                  <span
+                  >
+                    — {node.frontmatter.date}
+                  </span>
+                </h3>
+                <p>{node.excerpt}</p>
+              </div>
+            </Link>
+          )})}
+      </div>
+    </Layout>
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
