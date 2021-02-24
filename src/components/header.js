dcce3af43569d5b2d0903b3e59e48b60027f776c
Eleonora <eleonora.baret@gmail.com>
2021-02-24 14:27:28 +0100

    Added social icons header

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,27 @@ import React from "react";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import styles from "./header-css-modules.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faLinkedin, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({ siteTitle }) => (
   <header>
     <div className={styles.social}>
+      <div>
+        <a className={styles.icons} href='https://github.com/spaghetti-n5'>
+          <FontAwesomeIcon icon={faGithubSquare} size="lg" />
+        </a>
+        <a className={styles.icons} href='https://www.linkedin.com/in/eleonora-baret-4551a632'>
+          <FontAwesomeIcon icon={faLinkedin} size="lg" />
+        </a>
+        <a className={styles.icons} href='https://www.instagram.com/cucinadalnord.it/'>
+          <FontAwesomeIcon icon={faInstagramSquare} size="lg" />
+        </a>
+        <a className={styles.icons} href='mailto: cucinadalnord@gmail.com'>
+          <FontAwesomeIcon icon={faEnvelope} size="lg" />
+        </a>
+      </div>
       <Link to="/chi-sono">Chi sono</Link>
     </div>
     <div className={styles.header}>
