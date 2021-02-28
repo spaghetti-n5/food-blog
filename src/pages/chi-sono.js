caf545253c802155cb2ce53534a6e0eb75768138
Eleonora <eleonora.baret@gmail.com>
2021-02-28 12:15:05 +0100

    refactored image component to be reusable

--- src/pages/chi-sono.js
+++ src/pages/chi-sono.js
@@ -13,9 +13,9 @@ export default function About() {
             <p>Mi presento sono Eleonora, ho vissuto in diverse parti di Europa ed ora vivo a Berlino da un bel po’.</p>
             <p>La mia professione è tutt’altra ma adoro cucinare, soprattutto sperimentare piatti nuovi e legati a diverse nazionalità. I viaggi mi hanno aperto un mondo differente di sapori e profumi che cerco di inserire nei miei piatti, anche nelle ricette della tradizione italiana apportandone una sfumatura differente.</p>
             <p>Adoro la cucina del Sud est asiatico ed il mio ultimo viaggio in Thailandia mi ha permesso di assaporarne i sapori in loco e di riprodurli anche qui nella fredda città di Berlino.</p>
-            <p>Un’altra parte importante della mia cucina è l’influenza spagnola data dai mille viaggi nella penisola iberica ma soprattutto dal mio fidanzato, originario della Comunità Valenciana, che mi ha trasmesso le ricette delle più comuni tapas dei bar spagnoli ed i segreti per realizzare un’originale e buonissima paella.</p>
+            <p>Un’altra parte importante della mia cucina è l’influenza spagnola data dai mille viaggi nella penisola iberica ma soprattutto da mio marito, originario della Comunità Valenciana, che mi ha trasmesso le ricette delle più comuni tapas dei bar spagnoli ed i segreti per realizzare un’originale e buonissima paella.</p>
              <p>Terza ed ultima parte, non meno importante, rappresenta un tocco tradizionale dato da molti anni di esperienza in cucina, grazie alla mia mamma. Troverete quindi ricette casalinghe, dai sapori tradizionali e vivaci, caratterizzati da prodotti di stagione dell’orto, qualità nelle materie prime e ricercatezza nei profumi delle erbe aromatiche.</p>
-            <Image />
+            <Image alt="profile Image" filename="profile.jpg" />
             <p>Sapori, colori e profumi di un piatto rievocano momenti, ricordi, pensieri.</p>
         </div>
     </Layout>
