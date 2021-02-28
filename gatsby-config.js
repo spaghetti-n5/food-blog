3d3b00c156ecfa52e4532652bb655e6291a1572e
Eleonora <eleonora.baret@gmail.com>
2021-02-28 18:14:34 +0100

    cleanup config and removed ssr

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: `Cucina dal Nord`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `Eleonora Baret`,
   },
   plugins: [
